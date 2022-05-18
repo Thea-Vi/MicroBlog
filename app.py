65e5d08dd4ee5baf10493b7e14cc8224b5c5dceb
Thea-Vi <tei.villanueva@gmail.com>
2022-05-17 21:46:58 -0700

    Initialize flask

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-
-from flask import Flask
+import datetime
+from crypt import methods
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+entries = []
 
-@app.route("/")
-def hello_world():
-    return "hello, world"
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        date = datetime.datetime.today().strftime("%Y-%-m-%d")
+        entries.append((entry_content, date))
+      
+    
+    return render_template("home.html", entries=entries)
+
+
+if __name__=="__main__":
+    app.run(debug = True)
